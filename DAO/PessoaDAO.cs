0ce2be35e76564a6a4e19ba59ac77523e878f03e
MariRico <teolacio@gmail.com>
2021-11-15 22:22:14 -0300

    Teste

--- DAO/PessoaDAO.cs
+++ DAO/PessoaDAO.cs
@@ -1,4 +1,5 @@
 ﻿using PimVIII.Entidade;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -22,8 +23,7 @@ namespace PimVIII.DAO
             bool retorno = false;
             // Coloque o código aqui
 
-            using (SqlCommand cmd = new SqlCommand("INSERT INTO PESSOA( CPF, ENDERECO, NOME) " +
-                $"VALUES({p.cpf},{p.endereco},'{p.nome}');", conexao))
+            using (SqlCommand cmd = new SqlCommand($"INSERT INTO PESSOA( CPF, ENDERECO, NOME) VALUES({p.cpf},{p.endereco},'{p.nome}');", conexao))
             {
                 conexao.Open(); // abre a conexão com o banco
                 cmd.ExecuteNonQuery(); // executa cmd
@@ -46,9 +46,44 @@ namespace PimVIII.DAO
             SqlCommand cmd = new SqlCommand("SELECT * FROM PESSOA WHERE CPF = " + cpf, conexao);
             using (var dr = cmd.ExecuteReader())
             {
-                //pessoa.endereco = dr["endereco"]
+                if (dr.HasRows && dr.Read())
+                {
+                    EnderecoDAO enderecoDAO = new(conexao);
+
+                    pessoa.endereco = enderecoDAO.consulte(Convert.ToInt32(dr["endereco"]));
+                    pessoa.cpf = Convert.ToInt64(dr["endereco"]);
+                    pessoa.nome = dr["nome"].ToString();
+                    pessoa.telefones = null;
+                }
             }
             return pessoa;
+        }
+
+        private class EnderecoDAO
+        {
+            private SqlConnection conexao;
+            EnderecoDAO(SqlConnection pConexao)
+            {
+                conexao = pConexao;
+            }
+            public Endereco consulte(int intEndereco)
+            {
+                Endereco endereco = null;
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ENDERECO WHERE id = " + intEndereco, conexao);
+                using (var dr = cmd.ExecuteReader())
+                {
+                    if (dr.HasRows && dr.Read())
+                    {
+                        endereco.bairro = dr["bairro"].ToString();
+                        endereco.cep = Convert.ToInt32(dr["cep"]);
+                        endereco.cidade = dr["cidade"].ToString();
+                        endereco.estado = dr["estado"].ToString();
+                        endereco.logradouro = dr["logradouro"].ToString();
+                        endereco.numero = Convert.ToInt32(dr["numero"]);
+                    }
+                }
+                return endereco;
+            }
         }
     }
 }
